972683589e8a78cb886e1437d577f0672ec5b5db
gitong23 <gitong23@gmail.com>
2024-11-19 22:41:32 +0700

    [add] validator

--- src/controller/auth.controller.ts
+++ src/controller/auth.controller.ts
@@ -1,10 +1,25 @@
 import { Request, Response, NextFunction } from "express";
 import authService from "../services/auth.service";
+import { z } from "zod";
+import { validate } from "../utils/validation";
+
+export const loginSchema = z.object({
+  username: z.string().min(1, "Username is required"),
+  password: z.string().min(1, "Password is required"),
+});
+
+export const registerSchema = z.object({
+  username: z.string().min(4, "Username must be at least 4 characters long"),
+  password: z.string().min(6, "Password must be at least 6 characters long"),
+});
+
+// Infer the schema type
+export type RegisterSchema = z.infer<typeof registerSchema>;
 
 const register = async (req: Request, res: Response, next: NextFunction) => {
-  const { username, password } = req.body;
-
   try {
+    const validateData = validate(registerSchema, req.body);
+    const { username, password } = validateData;
     const result = await authService.register(username, password);
     res.status(201).json(result);
   } catch (error) {
@@ -13,9 +28,9 @@ const register = async (req: Request, res: Response, next: NextFunction) => {
 };
 
 const login = async (req: Request, res: Response, next: NextFunction) => {
-  const { username, password } = req.body;
-
   try {
+    const validateData = validate(loginSchema, req.body);
+    const { username, password } = validateData;
     const result = await authService.login(username, password);
     res.json(result);
   } catch (error) {
--- src/middleware/error.midleware.ts
+++ src/middleware/error.midleware.ts
@@ -1,10 +1,18 @@
 import { Request, Response, NextFunction } from 'express';
 import logger from '../utils/logger';
+import { ZodError } from 'zod';
 
 export const errorHandler = (err: any, req: Request, res: Response, next: NextFunction): void => {
 
-  const statusCode = err.status || 500;
-  const message = err.message || 'Internal Server Error';
+  let statusCode;
+  let message;
+  if (err instanceof ZodError){
+    statusCode = 400;
+    message = err.errors.map((error) => error.message).join(', ');
+  } else {
+    statusCode = err.status || 500;
+    message = err.message || 'Internal Server Error';
+  }
 
   logger.error(`${req.method} ${req.url} - ${message} - ${err.stack}`);
 
