629d2ba95ef729c2d041d59f566c4fcb2a19dadd
gitong23 <gitong23@gmail.com>
2024-11-19 22:49:14 +0700

    [fix] zod schema

--- src/controller/auth.controller.ts
+++ src/controller/auth.controller.ts
@@ -3,18 +3,18 @@ import authService from "../services/auth.service";
 import { z } from "zod";
 import { validate } from "../utils/validation";
 
-export const loginSchema = z.object({
+const loginSchema = z.object({
   username: z.string().min(1, "Username is required"),
   password: z.string().min(1, "Password is required"),
 });
 
-export const registerSchema = z.object({
+const registerSchema = z.object({
   username: z.string().min(4, "Username must be at least 4 characters long"),
   password: z.string().min(6, "Password must be at least 6 characters long"),
 });
 
 // Infer the schema type
-export type RegisterSchema = z.infer<typeof registerSchema>;
+// type RegisterSchema = z.infer<typeof registerSchema>;
 
 const register = async (req: Request, res: Response, next: NextFunction) => {
   try {
