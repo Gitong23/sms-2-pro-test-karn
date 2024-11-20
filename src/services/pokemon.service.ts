e0ff6818f7819940e59d09bb8e39a25084ccdf76
gitong23 <gitong23@gmail.com>
2024-11-21 00:41:17 +0700

    [add] cache in pokemon service

--- src/services/pokemon.service.ts
+++ src/services/pokemon.service.ts
@@ -1,21 +1,44 @@
-import { count } from "console";
 import Pokemon, { IPokemon } from "../models/pokemon.model";
+import { redisClient } from "../db/redis";
+import logger from "../utils/logger";
+
+//temp test
+const REDIS_TTL = 5; // cache for 10 minute
 
 interface IPokemonService {
   getByName(name: string): Promise<IPokemon | null>;
+  getAbilitiesByName(name: string): Promise<{ name: string; hidden: boolean }[] | null>;
+  getRandom(): Promise<IPokemon | null>;
 }
 
 class PokemonService implements IPokemonService {
+
+  private async getPokemonCache(name: string): Promise<IPokemon | null> {
+    const cacheKey = `pokemon:${name.toLowerCase()}`;
+    const cachedData = await redisClient?.get(cacheKey);
+    if (cachedData) {
+      logger.info(`Cache hit for Pokémon: ${name}`);
+      return JSON.parse(cachedData);
+    }
+
+    logger.info(`Cache miss for Pokémon: ${name}`);
+    //fetch from db on cache miss
+    const pokemon = await Pokemon.findOne({ name: { $regex: new RegExp(`^${name}$`, "i") } });
+    if (!pokemon) {
+      return null;
+    }
+
+    //write cache
+    await redisClient?.set(cacheKey, JSON.stringify(pokemon), { EX: REDIS_TTL });
+    return pokemon;
+  }
+
   async getByName(name: string): Promise<IPokemon | null> {
-    return Pokemon.findOne({ name: { $regex: new RegExp(`^${name}$`, "i") } });
+    return this.getPokemonCache(name);
   }
 
   async getAbilitiesByName(name: string): Promise<{ name: string; hidden: boolean }[] | null> {
-    const pokemon = await Pokemon.findOne(
-      { name: { $regex: new RegExp(`^${name}$`, 'i') } },
-      { abilities: 1, _id: 0 }
-    );
-
+    const pokemon = await this.getPokemonCache(name);
     return pokemon?.abilities || null;
   }
 
@@ -24,7 +47,11 @@ class PokemonService implements IPokemonService {
     if(total === 0) return null;
 
     const randomIdx = Math.floor(Math.random() * total);
-    return Pokemon.findOne().skip(randomIdx);
+    const randomName = await Pokemon.findOne({}, { name: 1 }).skip(randomIdx);
+
+    if (!randomName) return null;
+    
+    return this.getPokemonCache(randomName.name);
   }
 }
 
